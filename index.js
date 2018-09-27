1352bcaef37c741286b4c66d306e7bc899b236e5
jcoleman-techempower <jcoleman@techempower.com>
2018-09-27 13:47:42 -0700

    Begin update for Hapi 17

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ const requireAll = require("require-all");
 const packageAttributes = require("./package.json");
 
 const transformRoutes = routes => traverse(routes)
-    .reduce((apiRoutes, routeDefinition) => {
-        const isRoute = _.has(routeDefinition, "method") && _.has(routeDefinition, "path");
-        if (isRoute) {
-            apiRoutes.push(routeDefinition);
-        }
-        return apiRoutes;
-    }, []);
+  .reduce((apiRoutes, routeDefinition) => {
+    const isRoute = _.has(routeDefinition, "method") && _.has(routeDefinition, "path");
+    if (isRoute) {
+      apiRoutes.push(routeDefinition);
+    }
+    return apiRoutes;
+  }, []);
 
 const setUpRoutes = directory =>
-    (server, options, next) => {
-        const routes = requireAll(directory);
-        server.route(transformRoutes(routes));
-        next();
-    };
+  (server, options) => {
+    const routes = requireAll(directory);
+    return server.route(transformRoutes(routes));
+  };
 
 module.exports = directory => ({
-    register: _.merge(
-        setUpRoutes(directory),
-        {
-            attributes: {
-                pkg: packageAttributes,
-            },
-        }
-    ),
+  register: _.merge(
+    setUpRoutes(directory),
+    {
+      attributes: {
+        pkg: packageAttributes,
+      },
+    }
+  ),
+  name: "hapi-route-autoloader",
 });
